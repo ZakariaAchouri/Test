024f81d4e0c9ee37de09ae7c01c90c17a4af512c
ZakariaAchouri <zakaria_achouri@hotmail.co.uk>
2017-07-12 15:58:37 +0100

    Hello tomato

---
+++ number chase.py
@@ -0,0 +1,37 @@
+import random
+import time
+
+
+n = random.randint(-10000,10000)
+
+x = True
+
+m = random.randint(-10000,10000)
+while x == True:
+    if m == n:
+
+        print("yay!")
+        break
+
+    else:
+        if m < n:
+
+            m = m + random.randint(0,100)
+
+            n = n + random.randint(-100,100)
+
+        if m > n:
+
+            m = m - random.randint(0,100)
+
+            n = n + random.randint(-100,100)
+    time.sleep(0.0000000000000000001)
+    print(n,"_-_-_-_",m)
+    if n > 100000:
+
+            n = n - random.randint(0,100)
+
+    if n < -100000:
+
+            n = n + random.randint(0,100)
+
